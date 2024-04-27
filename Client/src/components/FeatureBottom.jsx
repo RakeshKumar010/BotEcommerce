c92a448ec51bb9f2abfa5a1b0046f8e8fdea4ba2
rakesh <rakeshkumar748844@gmail.com>
2024-04-27 09:27:54 +0530

    'add-some-info'

--- Client/src/components/FeatureBottom.jsx
+++ Client/src/components/FeatureBottom.jsx
@@ -1,34 +1,34 @@
 import { PiAirplaneTiltLight } from "react-icons/pi";
 import { CiBadgeDollar } from "react-icons/ci";
 import { CiLock } from "react-icons/ci";
-import { AiOutlineShopping } from "react-icons/ai";
+import { CiShoppingCart } from "react-icons/ci";
 
 const FeatureBottom = () => {
   return (
-    <div className="bg-[#eeeeee] flex justify-around items-center py-10 flex-wrap">
-      <div className="flex gap-4 items-center">
+    <div className="bg-[#eeeeee] flex justify-around gap-4 items-center py-10 flex-wrap">
+      <div className="flex gap-4 items-center p-2">
         <PiAirplaneTiltLight className="text-5xl"/>
 
         <p className="text-lg">
           Woldwide <br /> Shipping
         </p>
       </div>
-      <div className="flex gap-4 items-center">
+      <div className="flex gap-4 items-center p-2">
         <CiBadgeDollar className="text-5xl"/>
 
         <p className="text-lg">
           Quality <br /> Guarantee
         </p>
       </div>
-      <div className="flex gap-4 items-center">
+      <div className="flex gap-4 items-center p-2">
         <CiLock className="text-5xl"/>
         <p className="text-lg">
           Secure <br /> Guarantee
         </p>
       </div>
 
-      <div className="flex gap-4 items-center">
-        <AiOutlineShopping className="text-5xl"/>
+      <div className="flex gap-4 items-center p-2">
+        <CiShoppingCart className="text-5xl font-thin"/>
         <p className="text-lg">
           Secure <br /> Shopping
         </p>
