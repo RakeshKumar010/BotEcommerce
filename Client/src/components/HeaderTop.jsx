760266ea15eb2ff7c64e8f949dcf22bbd2eca5d1
rakesh <rakeshkumar748844@gmail.com>
2024-04-27 09:18:23 +0530

    'add-some-new-page'

--- Client/src/components/HeaderTop.jsx
+++ Client/src/components/HeaderTop.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const HeaderTop = () => {
   return (
-    <div className="bg-uiColor text-white flex px-8 p-1 uppercase">
-      <p>WINTER SALE LIVE NOW</p>
+    <div className=" bg-uiColor  text-white flex px-8 p-2 uppercase">
+      <p className="header-top-animation text-sm">WINTER SALE LIVE NOW</p>
     </div>
   );
 };
