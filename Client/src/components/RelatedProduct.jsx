80d8bae64e3c39fa262216ceed380adb6b07020b
rakesh <rakeshkumar748844@gmail.com>
2024-05-15 19:43:49 +0530

    add-slider

--- Client/src/components/RelatedProduct.jsx
+++ Client/src/components/RelatedProduct.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
 import MiniCard from "./MiniCard";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const RelatedProduct = () => {
-  const [data, setData] = useState();
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const getFun = async () => {
@@ -12,17 +16,60 @@ const RelatedProduct = () => {
     };
     getFun();
   }, []);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 800,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
-    <div className=" px-5">
+    <div className="px-5 overflow-hidden">
       <p className="font-semibold text-2xl text-center my-6 mt-10 md:my-10 md:mt-40">
         You May Also Like
       </p>
-      <div className="flex justify-center md:flex-nowrap flex-wrap gap-5 ">
-        {data &&
-          data.slice(0, 6).map((value, index) => {
-            return <MiniCard value={value} key={index} />;
-          })}
-      </div>
+      {error ? (
+        <p className="text-center text-red-500">Error: {error}</p>
+      ) : data.length === 0 ? (
+        <p className="text-center">Loading...</p>
+      ) : (
+        <Slider {...settings}>
+          {data.slice(0, 6).map((value, index) => (
+            <MiniCard key={index} value={value} />
+          ))}
+        </Slider>
+      )}
     </div>
   );
 };
