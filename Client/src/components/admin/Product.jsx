a0ed867c4d5266553ae4e5e032249402c73ba52a
rakesh <rakeshkumar748844@gmail.com>
2024-06-10 11:01:46 +0530

    add-some-page

--- Client/src/components/admin/Product.jsx
+++ Client/src/components/admin/Product.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
-
+import * as XLSX from 'xlsx';
 const Product = () => {
   const [data, setData] = useState();
  
@@ -11,18 +11,29 @@ const Product = () => {
       let result = await fetch("https://psyrealestate.in/product");
       result = await result.json();
       setData(result);
-     
+  
+   
     };
     getFun();
   }, [pageLoad]);
 
+  const downloadExcal =()=>{
+   // Convert the result to a worksheet
+   const ws = XLSX.utils.json_to_sheet(data);
+   // Create a new workbook
+   const wb = XLSX.utils.book_new();
+   // Append the worksheet to the workbook
+   XLSX.utils.book_append_sheet(wb, ws, "Products");
+   // Write the workbook to a file
+   XLSX.writeFile(wb, "ProductsData.xlsx");
+  }
   return (
     <>
     <div className="absolute right-0 border-dotted border-black border-0 md:border-2 min-h-screen w-full lg:w-[82%] px-2">
       <div className="text-center py-5">
         <p className="font-bold text-3xl text-teal-400">All Products</p>
         <div className="flex justify-end">
-          <p className="bg-teal-400 text-white p-5 py-3 rounded-md mx-2 hover:scale-105 transition-all duration-200 hover:shadow-lg">
+          <p onClick={downloadExcal} className="bg-teal-400 cursor-pointer text-white p-5 py-3 rounded-md mx-2 hover:scale-105 transition-all duration-200 hover:shadow-lg">
             Export to Excel
           </p>
         </div>
