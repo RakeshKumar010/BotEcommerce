fd34eec5bc579a2dd6142f50639e17f0b07a2076
rakesh <rakeshkumar748844@gmail.com>
2024-06-17 17:19:29 +0530

    add-some

--- Client/src/components/HeaderTop.jsx
+++ Client/src/components/HeaderTop.jsx
@@ -1,14 +1,40 @@
-import { ApiColor } from "./api/data";
-
+import { useEffect, useState } from "react";
 
 const HeaderTop = () => {
-  
+  const [apiDataColor, setApiDataColor] = useState();
+  useEffect(() => {
+ 
+    function trimUrl(url) {
+      const parsedUrl = new URL(url);
+      return parsedUrl.hostname + (parsedUrl.port ? ":" + parsedUrl.port : "");
+    }
+    async function getColor() {
+      const currentUrl = trimUrl(window.location.href);
+      let response = await fetch(
+        "https://psyrealestate.in/client/" + currentUrl
+      );
+      const clientData = await response.json();
+
+      response = await fetch("https://psyrealestate.in/color");
+      const colorsData = await response.json();
+
+      const filteredResults = colorsData.filter((value) => {
+        // console.log(value.clientId);
+        return value.clientId == clientData._id;
+      });
+     const  ApiColor = filteredResults[filteredResults.length - 1].color;
+     setApiDataColor(ApiColor)
+    }
+    getColor();
+  });
   return (
     <>
-      <div style={{backgroundColor:ApiColor}} className="text-white flex px-8 p-2 uppercase">
+      <div
+        style={{ backgroundColor: apiDataColor }}
+        className="text-white flex px-8 p-2 uppercase"
+      >
         <p className="header-top-animation text-sm"> 🌟 50% OFF SALE! 🌟 </p>
       </div>
-      
     </>
   );
 };
