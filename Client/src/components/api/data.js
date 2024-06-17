ebe5111150a4cb8b64388899e367a5c3b137238d
rakesh <rakeshkumar748844@gmail.com>
2024-06-17 14:40:32 +0530

    add-some

--- Client/src/components/api/data.js
+++ Client/src/components/api/data.js
@@ -1,20 +1,20 @@
 let ApiColor = "";
-
+function trimUrl(url) {
+  const parsedUrl = new URL(url);
+  return parsedUrl.hostname + (parsedUrl.port ? ":" + parsedUrl.port : "");
+}
 async function getColor() {
-  function trimUrl(url) {
-    const parsedUrl = new URL(url);
-    return parsedUrl.hostname + (parsedUrl.port ? ":" + parsedUrl.port : "");
-  }
-  let currentUrl = window.location.href;
-  currentUrl = trimUrl(currentUrl);
-  let result2 = await fetch("https://psyrealestate.in/client/" + currentUrl);
-  result2 = await result2.json();
-  let result = await fetch("https://psyrealestate.in/color");
-  result = await result.json();
+ 
+  const currentUrl = trimUrl(window.location.href);
+  let response = await fetch("https://psyrealestate.in/client/" + currentUrl);
+  const clientData = await response.json();
+  
+  response = await fetch("https://psyrealestate.in/color");
+  const colorsData = await response.json();
 
-  const filteredResults = result.filter((value) => {
+  const filteredResults = colorsData.filter((value) => {
     // console.log(value.clientId);
-    return value.clientId == result2._id;
+    return value.clientId == clientData._id;
   });
   ApiColor = filteredResults[filteredResults.length - 1].color;
   console.log(ApiColor);
