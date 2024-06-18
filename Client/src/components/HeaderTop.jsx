909e00ad05f611c7d435e1febc9f456a30ef4ca1
rakesh <rakeshkumar748844@gmail.com>
2024-06-18 19:45:48 +0530

    add-some

--- Client/src/components/HeaderTop.jsx
+++ Client/src/components/HeaderTop.jsx
@@ -1,41 +1,42 @@
 import { useEffect, useState } from "react";
+import { ApiColor } from "./api/data";
 
 const HeaderTop = () => {
-  const [apiDataColor, setApiDataColor] = useState();
-  useEffect(() => {
-    function trimUrl(url) {
-      const parsedUrl = new URL(url);
-      return parsedUrl.hostname + (parsedUrl.port ? ":" + parsedUrl.port : "");
-    }
-    async function getColor() {
-      const currentUrl = trimUrl(window.location.href);
-      let response = await fetch(
-        "https://psyrealestate.in/client/" + currentUrl
-      );
-      const clientData = await response.json();
+  // const [apiDataColor, setApiDataColor] = useState();
+  // useEffect(() => {
+  //   function trimUrl(url) {
+  //     const parsedUrl = new URL(url);
+  //     return parsedUrl.hostname + (parsedUrl.port ? ":" + parsedUrl.port : "");
+  //   }
+  //   async function getColor() {
+  //     const currentUrl = trimUrl(window.location.href);
+  //     let response = await fetch(
+  //       "https://psyrealestate.in/client/" + currentUrl
+  //     );
+  //     const clientData = await response.json();
 
-      response = await fetch("https://psyrealestate.in/color");
-      const colorsData = await response.json();
+  //     response = await fetch("https://psyrealestate.in/color");
+  //     const colorsData = await response.json();
 
-      const filteredResults = colorsData.filter((value) => {
-        // console.log(value.clientId);
-        return value.clientId == clientData._id;
-      });
-      if(filteredResults.length>0){
+  //     const filteredResults = colorsData.filter((value) => {
+  //       // console.log(value.clientId);
+  //       return value.clientId == clientData._id;
+  //     });
+  //     if(filteredResults.length>0){
 
-        setApiDataColor(filteredResults[0].color);
-      }else{
-        setApiDataColor('black')
-      }
-      //  const  ApiColor = filteredResults[filteredResults.length - 1].color;
-      //  setApiDataColor(ApiColor)
-    }
-    getColor();
-  });
+  //       setApiDataColor(filteredResults[0].color);
+  //     }else{
+  //       setApiDataColor('black')
+  //     }
+  //     //  const  ApiColor = filteredResults[filteredResults.length - 1].color;
+  //     //  setApiDataColor(ApiColor)
+  //   }
+  //   getColor();
+  // });
   return (
     <>
       <div
-        style={{ backgroundColor: apiDataColor }}
+          style={ApiColor?{ backgroundColor: ApiColor }:{ backgroundColor: 'black' }}
         className="text-white flex px-8 p-2 uppercase"
       >
         <p className="header-top-animation text-sm"> 🌟 50% OFF SALE! 🌟 </p>
